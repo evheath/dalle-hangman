77350245fe724f7066ac3b717498a6398dcf7b3c
evheath <elliot.v.heath@gmail.com>
2022-06-13 15:19:22 -0500

    three columns on lobby

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{html,ts}'],
-  darkMode: false,
   content: [
     "./src/**/*.{html,ts}",
   ],
