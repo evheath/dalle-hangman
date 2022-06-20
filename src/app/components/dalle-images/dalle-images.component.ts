5e96165275d5db5ad6625ddf873ffcb83d28657d
evheath <elliot.v.heath@gmail.com>
2022-06-20 09:45:58 -0500

    displaying one set of dalle images

--- src/app/components/dalle-images/dalle-images.component.ts
+++ src/app/components/dalle-images/dalle-images.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { Observable } from 'rxjs';
+import { LobbyService } from 'src/app/services/lobby.service';
 
 @Component({
   selector: 'app-dalle-images',
@@ -9,27 +10,38 @@ import { Observable } from 'rxjs';
 })
 export class DalleImagesComponent implements OnInit {
 
-  public urls: Observable<string | null>[] = [];
-
-  @Input() dalleId: string = "";
+  public urls$: Observable<any>[] = [];
+  private dalleId: string = "";
 
   constructor(
-    public storage: AngularFireStorage
-  ) { }
+    public storage: AngularFireStorage,
+    private lobbyService: LobbyService
+  ) {
+    this.lobbyService.lobbyDoc$.subscribe(lobby => {
+      if (lobby) {
+        if (lobby.dalleId != this.dalleId) {
+          this.newDalleId(lobby.dalleId);
+        }
 
-  ngOnInit(): void {
-    // console.log(`DalleImagesComponent.ngOnInit()`);
-    for (let i = 1; i <= 9; i++) {
-      this.urls.push(this.getUrl(i));
-    }
+      }
+    })
   }
 
-  /**
-   * getUrl
-   */
-  public getUrl(number: number): Observable<string | null> {
+  ngOnInit(): void {
+  }
+
+  public getUrl(number: number): Observable<any> {
     let location = `${this.dalleId}/${number}.jpg`
     return this.storage.ref(location).getDownloadURL();
+  }
+
+  private newDalleId(newId: string) {
+    this.dalleId = newId;
+    this.urls$ = [];
+    for (let index = 1; index < 10; index++) {
+      this.urls$.push(this.getUrl(index));
+
+    }
   }
 
 }
