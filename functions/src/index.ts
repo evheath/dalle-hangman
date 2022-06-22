19c375a65b4a78a5f726f2591453b3ffb30451b4
evheath <elliot.v.heath@gmail.com>
2022-06-22 10:35:17 -0500

    backend getting dalle images

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,21 @@
-// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
+import axios from "axios";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const onDalleDocumentCreation = functions
+    .runWith({timeoutSeconds: 300})
+    .firestore.document("dalle/{dalleId}")
+    .onCreate(async (snapshot, context) => {
+      const {prompt} = snapshot.data();
+      const data = {
+        prompt,
+      };
+      const options = {
+        headers: {
+          "Content-Type": "application/json",
+          "Connection": "keep-alive",
+        },
+      };
+      const res = await axios.post("https://backend.craiyon.com/generate", data, options);
+      const {images} = res.data;
+      return snapshot.ref.update({images});
+    });
