40d18d8916add6bdd5243a45a3ee92c5d942c798
Elliot Heath <elliot@businessfactory.dev>
2025-04-12 14:16:17 -0500

    using the new dicebear endpoint

--- src/app/pipes/adventurer-url.pipe.ts
+++ src/app/pipes/adventurer-url.pipe.ts
@@ -1,18 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { faker } from '@faker-js/faker';
 
+
 @Pipe({
   name: 'adventurerUrl'
 })
 export class AdventurerUrlPipe implements PipeTransform {
 
   transform(word: string | null): string {
-    if (!word) {
-      return `https://avatars.dicebear.com/api/adventurer/'${faker.name.findName()}.svg`;
-    } else {
-      return `https://avatars.dicebear.com/api/adventurer/'${word}.svg`;
-
-    }
+    return `https://api.dicebear.com/9.x/fun-emoji/svg?seed=${word ?? faker.name.findName()}`;
   }
 
 }
